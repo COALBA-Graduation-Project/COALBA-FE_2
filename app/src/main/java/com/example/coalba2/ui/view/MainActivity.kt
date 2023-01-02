ced3675021bea737a2f64e230ec494755365e926
cyj2825 <cyj2825@naver.com>
2023-01-02 18:20:51 +0900

    ✨ feature : BottomNavigation & 상단바 구현 완료
    BottomNavigation 구현 및 MainActivity에 띄울 상단바 구현 완료
    
    > Related to : #3

--- app/src/main/java/com/example/coalba2/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/coalba2/ui/view/MainActivity.kt
@@ -2,11 +2,87 @@ package com.example.coalba2.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.coalba2.R
+import com.example.coalba2.databinding.ActivityMainBinding
+import com.example.coalba2.ui.fragment.HomeFragment
+import com.example.coalba2.ui.fragment.MessageFragment
+import com.example.coalba2.ui.fragment.MypageFragment
+import com.example.coalba2.ui.fragment.WorkspaceFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요없이 편의성을 위해 바인딩 변수 재선언
+    private val binding get() = mBinding!!
+    lateinit var bnMain : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // 바인딩
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 커스텀한 toolbar를 액션바로 사용
+        setSupportActionBar(binding.tbMain)
+        // toolbar 타이틀 제거
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setLogo(R.drawable.ic_logo)
+        supportActionBar?.setDisplayUseLogoEnabled(true)
+        loadFragment(HomeFragment())
+        bnMain = binding.bnMain
+        bnMain.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.workspace -> {
+                    loadFragment(WorkspaceFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(MessageFragment())
+                    true
+                }
+                R.id.mypage -> {
+                    loadFragment(MypageFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // 액션버튼 메뉴 액션바에 집어넣기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+    // 액션버튼 클릭 했을 때
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.notice -> {
+                Toast.makeText(applicationContext, "알림 화면으로!", Toast.LENGTH_SHORT).show()
+                return super.onOptionsItemSelected(item)
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun loadFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fl_main, fragment).commit()
+    }
+
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 함
+        mBinding = null
+        super.onDestroy()
     }
 }
