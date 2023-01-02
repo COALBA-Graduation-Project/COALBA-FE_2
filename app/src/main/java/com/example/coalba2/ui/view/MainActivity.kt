8e6880d57940634ff499eae42faaaf5ef339f3a7
cyj2825 <cyj2825@naver.com>
2023-01-02 15:09:12 +0900

    🚚 move : MainActivity 패키지 변경
    MainActivity 패키지 변경

--- app/src/main/java/com/example/coalba2/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/coalba2/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.coalba2
+package com.example.coalba2.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coalba2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
